156877958091a8213d7e196bddf52536334610ac
iiyson <travelerc@icloud.com>
2022-12-23 17:19:21 +0800

    dva

--- backend/application/routing.py
+++ backend/application/routing.py
@@ -7,6 +7,6 @@ from application.websocketConfig import MegCenter,realTimeChatConsumer
 websocket_urlpatterns = [
     path('ws/<str:service_uid>/', MegCenter.as_asgi()), #consumers.DvadminWebSocket 是该路由的消费者
     # path('asr/<str:room_name>/',realTimeChatConsumer.as_asgi()),
-    re_path(r'^ws/asr/(?P<room_name>\w+)/$',realTimeChatConsumer.as_asgi())
+    re_path(r'^ws/asr/(?P<room_id>\w+)/$',realTimeChatConsumer.as_asgi())
 ]
 
--- backend/application/websocketConfig.py
+++ backend/application/websocketConfig.py
@@ -212,7 +212,7 @@ class realTimeChatConsumer(AsyncWebsocketConsumer):
 # 测试用
 # class realTimeChatConsumer(AsyncWebsocketConsumer):
 #     async def connect(self):
-#         self.room_name=self.scope['url_route']['kwargs']['room_name']
+#         self.room_name=self.scope['url_route']['kwargs']['room_id']
 #         # self.room_group_name=self.room_name
 #         self.record=''
 #         await self.channel_layer.group_add(self.room_name,self.channel_name )
